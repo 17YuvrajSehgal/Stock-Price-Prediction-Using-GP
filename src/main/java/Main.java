5251171598b7e221de281400fe1506f32eb18759
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-02-23 17:17:07 -0500

    Added file reader to read stock data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,22 @@
+import StockPredictor.Stock;
 import ec.Evolve;
+
+import java.nio.file.Path;
 
 /**
  * This is the main class
  */
 public class Main {
     public static void main(String[] args) {
-        String pathToFiles = "src/main/results/";
-        int numberOfJobs = 10;
-        String statisticType = "ec.gp.koza.KozaShortStatistics";
-        String[] runConfig = new String[] {
-                Evolve.A_FILE, "src/parameterFiles/stock.params",
-                //"-p", ("stat="+statisticType),
-                "-p", ("stat.file=$"+pathToFiles+"out.stat"),
-                "-p", ("jobs="+numberOfJobs)
-        };
-        Evolve.main(runConfig);
+//        String pathToFiles = "src/main/results/";
+//        int numberOfJobs = 10;
+//        String statisticType = "ec.gp.koza.KozaShortStatistics";
+//        String[] runConfig = new String[] {
+//                Evolve.A_FILE, "src/parameterFiles/stock.params",
+//                //"-p", ("stat="+statisticType),
+//                "-p", ("stat.file=$"+pathToFiles+"out.stat"),
+//                "-p", ("jobs="+numberOfJobs)
+//        };
+//        Evolve.main(runConfig);
     }
 }
--- src/main/java/StockPredictor/Stock.java
+++ src/main/java/StockPredictor/Stock.java
@@ -5,7 +5,10 @@ import ec.Individual;
 import ec.gp.GPProblem;
 import ec.simple.SimpleProblemForm;
 
+import java.io.*;
+import java.nio.file.Path;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Stock extends GPProblem implements SimpleProblemForm {
 
@@ -14,6 +17,25 @@ public class Stock extends GPProblem implements SimpleProblemForm {
     public long volume;
     @Override
     public void evaluate(EvolutionState evolutionState, Individual individual, int i, int i1) {
+
+    }
+
+    public void fileReader(String path){
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Split the line by tab (,) delimiter
+                String[] parts = line.split(",");
+                // Print each part
+                for (String part : parts) {
+                    System.out.print(part + "\t");
+                }
+                System.out.println(); // Move to the next line
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
